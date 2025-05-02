724223fd797f122883f07e852284542e77bb2a68
Vaishnavi Singh <vaishnavi.singh@EMEDITEK.NET>
2025-05-02 18:31:54 +0530

    Your message describing the changes

--- contact-manager/src/App.jsx
+++ contact-manager/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import AddContact from "./Pages/AddContact";
 import EditContact from "./Pages/EditContact";
@@ -6,6 +6,11 @@ import EditContact from "./Pages/EditContact";
 function App(){
   return(
     <BrowserRouter>
+    <nav style={{padding: "10px" ,borderBottom:"1px solid #ccc"}}>
+      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+      <Link to="/AddContact">Add Contact</Link>
+      <Link to="/EditContact">EditContact </Link>
+    </nav>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/AddContact" element={<AddContact/>}/>
